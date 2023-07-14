c71ea2237ed7e1258aa4cd125ecba6bd357d8057
MikaSchweingruber <mikaschwein@gmail.com>
2023-07-14 15:22:31 +0200

    jhytg

--- meme_api/meme_api/settings.py
+++ meme_api/meme_api/settings.py
@@ -30,6 +30,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+REST_FRAMEWORK = {
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer'
+    ]
+}
 
 # Application definition
 
