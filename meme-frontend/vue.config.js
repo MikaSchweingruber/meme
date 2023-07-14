b026bd0ffcca66fe76951e73ee2935073e3a7cba
MikaSchweingruber <mikaschwein@gmail.com>
2023-07-14 15:05:32 +0200

    rest

--- meme-frontend/vue.config.js
+++ meme-frontend/vue.config.js
@@ -2,5 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  devServer: {
+    proxy: {
+      "^/(admin|api|static|blocklist)": {
+        target: "http://127.0.0.1:8000"
+      }
+    }
+  },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Memes";
+            return args;
+        })
+    }
+}
+)
